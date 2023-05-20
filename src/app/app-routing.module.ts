f9b8a7aaef55d0b0ac7643c42b295584e6d82047
Guilherme Campos de Melo <legitzerasoulegitmsm@gmail.com>
2023-05-20 13:44:08 -0300

    agendamento page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,15 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },  {
+    path: 'agendamento',
+    loadChildren: () => import('./pages/agendamento/agendamento.module').then( m => m.AgendamentoPageModule)
   },
+  {
+    path: 'add-agendamento',
+    loadChildren: () => import('./pages/add-agendamento/add-agendamento.module').then( m => m.AddAgendamentoPageModule)
+  },
+
 
 ];
 

cebabcbef9aa553f74804777b506d40ef6dbdb0a
Your Name <you@example.com>
2023-08-22 08:41:02 -0300

    CHORE add-agenda: select of servico

---
+++ ionic/src/app/model/servico.ts
@@ -0,0 +1,19 @@
+export class Servico {
+    id: number;
+    nome: string;
+    descricao: string;
+
+    // constructor() {
+    //     this.id = 0;
+    //     this.nome = "";
+    //     this.descricao = "";
+    // }
+    constructor(nome: string, desc: string) {
+        this.id = 0;
+        this.nome = nome;
+        this.descricao = desc;
+    }
+
+
+
+}
